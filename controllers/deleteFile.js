a522f27ff42f2c607b52c325534e24148a5173c1
rohanrakeshjain26 <rohanrakeshjain26@gmail.com>
2021-02-14 21:25:27 +0530

    Created deleteFile controller to handle logic to delete file.

---
+++ controllers/deleteFile.js
@@ -0,0 +1,76 @@
+const jwt = require('jsonwebtoken');
+const s3 = require('./s3obj').s3obj;
+
+const FileModel = require('../models/file');
+
+/**
+ * Used to delete the file
+ */
+exports.deleteFile = (req, res) => {
+    const decoded = jwt.verify(
+        req.headers['authorization'].split(' ')[1],
+        process.env.SECRET_KEY
+    );
+
+    FileModel.startSession()
+        .then((session) => {
+            session.startTransaction();
+            const userId = decoded._id;
+            const fileName = req.body.fileName;
+
+            FileModel.findOne({ user_id: userId, file_name: fileName })
+                .then((data) => {
+                    if (data) {
+                        const params = {
+                            Bucket: process.env.AWSBucketName,
+                            Key: fileName,
+                        };
+
+                        FileModel.deleteOne(
+                            {
+                                user_id: userId,
+                                file_name: fileName,
+                            },
+                            { session }
+                        )
+                            .then((fileData) => {
+                                // delete object form s3
+                                s3.deleteObject(
+                                    params,
+                                    async function (err, data) {
+                                        if (err) {
+                                            //  If fail, rollback transaction
+                                            await session.abortTransaction();
+                                            session.endSession();
+                                            return res.status(500).json({ msg: s3Err });
+                                        }
+                                        // If successfull, commmit transaction
+                                        await session.commitTransaction();
+                                        session.endSession();
+                                        res.json({
+                                            msg: 'File Successfully Deleted!',
+                                        });
+                                    }
+                                );
+                            })
+                            .catch((err) => {
+                                session.endSession();
+                                res.status(500).json({ msg: err });
+                            });
+                    } else {
+                        // Bad request
+                        session.endSession();
+                        res.status(400).json({
+                            msg: 'No such file is present.',
+                        });
+                    }
+                })
+                .catch((err) => {
+                    session.endSession();
+                    res.status(500).json({ msg: err });
+                });
+        })
+        .catch((err) => {
+            res.status(500).json({ msg: err });
+        });
+};

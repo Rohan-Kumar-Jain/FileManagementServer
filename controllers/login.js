abae7983a5049c249fac564d562c14a8f7b84fe0
rohanrakeshjain26 <rohanrakeshjain26@gmail.com>
2021-02-15 12:53:55 +0530

    Removed old code which uses promises.

--- controllers/login.js
+++ controllers/login.js
@@ -32,32 +32,3 @@ exports.login = async (req, res) => {
         res.status(500).json({ msg: err });
     }
 };
-
-// // Using promises
-// exports.login = (req, res) => {
-//     userModel.findOne({email: req.body.email})
-//         .then((user) => {
-//             if (user) {
-//                 if (bcrypt.compareSync(req.body.password, user.password)) {
-//                     // Passwords match
-//                     const payload = {
-//                         _id: user._id,
-//                         username: user.username,
-//                         email: user.email,
-//                     };
-//                     const token = jwt.sign(payload, process.env.SECRET_KEY, {
-//                         expiresIn: process.env.EXPIRES_IN,
-//                     });
-//                     res.json({ token: token });
-//                 } else {
-//                     // Passwords don't match
-//                     res.status(400).json({ msg: 'Password does not Match' });
-//                 }
-//             } else {
-//                 res.status(400).json({ msg: 'User does not exist' });
-//             }
-//         })
-//         .catch((err) => {
-//             res.status(500).json({ msg: err });
-//         });
-// };
